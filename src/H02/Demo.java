b8ca4c1d21acef44c8e38d7c61098cc35f8081ad
Daan645 <daan.jacobs@student.rocvf.nl>
2019-09-03 13:40:53 +0100

    Hoofdstuk 2 toegevoegd

---
+++ src/H02/Demo.java
@@ -0,0 +1,15 @@
+package H02;
+
+import java.applet.*;
+        import java.awt.*;
+
+public class Demo extends Applet {
+
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Kechbatron3Doezoe", 40, 40);
+
+    }
+}
